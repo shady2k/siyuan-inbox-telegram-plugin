6f4e1556a062d557ae1398221ff7fd67721c559f
Korachentsov Alexandr <shady2k@gmail.com>
2024-04-29 20:06:52 +0300

    Initial commit

---
+++ src/libs/index.d.ts
@@ -0,0 +1,23 @@
+type TSettingItemType = "checkbox" | "select" | "textinput" | "textarea" | "number" | "slider" | "button" | "hint";
+interface ISettingItem {
+    key: string;
+    value: any;
+    type: TSettingItemType;
+    title: string;
+    description?: string;
+    placeholder?: string;
+    slider?: {
+        min: number;
+        max: number;
+        step: number;
+    };
+    options?: { [key: string | number]: string };
+    action?: {
+        callback: () => void;
+    }
+    button?: {
+        label: string;
+        callback: () => void;
+    }
+    createElement?: (currentVal: any) => HTMLElement;
+}
